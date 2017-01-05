20307548b23c786f3bc3480acaad599cb30651fa
Rock <rock.zhang.cn@gmail.com>
2017-01-05 14:28:06 +0800

    增加查看指纹的方法

--- openssl.sh
+++ openssl.sh
@@ -1,3 +1,6 @@
 #!/bin/bash
 #查看证书信息
 openssl x509 -inform der  -noout -text -in 1.crt
+#查看Windows上显示的SHA1指纹。
+openssl dgst -sha1 121.201.98.2_443_1.crt 
+#SHA1(121.201.98.2_443_1.crt)= 73d39eec4c58a81d6b54a4a75f1ba9d90d618569
